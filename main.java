59abbdee4f0922486dc5482428c3e56a2d721bb3
Josh Gaviola <martin100906@gmail.com>
2023-02-19 22:29:18 +0800

    git commit -m "Updated guess.java" git push origin main

--- main.java
+++ main.java
@@ -4,23 +4,21 @@ import java.util.Random;
 import javax.swing.*;
 
 public class main {
+    private final JLabel textfield = new JLabel();
+    private final JFrame frame = new JFrame("Tic Tac Toe");
+    private final Random random = new Random();
+    private final JPanel title_panel = new JPanel();
+    private final JPanel button_panel = new JPanel();
+    private final JButton[] buttons = new JButton[9];
+    private int turn; // 0 is player, 1 is AI
+    private boolean gameOver = false;
+    private final String player = "Player";
+    private final String ai = "AI";
+    private int playerScore = 0;
+    private int aiScore = 0;
+    private int turnCount;
 
-    JLabel textfield = new JLabel();
-    JFrame frame = new JFrame("Tic Tac Toe");
-    Random random = new Random();
-    JPanel title_panel = new JPanel();
-    JPanel button_panel = new JPanel();
-    JButton[] buttons = new JButton[9];
-    int turn; // 0 is player, 1 is AI
-    boolean gameOver = false;
-    String player = "Player";
-    String ai = "AI";
-    int playerScore = 0;
-    int aiScore = 0;
-    int turnCount;
-    int currentTurn;
-
-    main() {
+    public main() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 800);
         frame.getContentPane().setBackground(new Color(50, 50, 50));
@@ -28,7 +26,7 @@ public class main {
         frame.setVisible(true);
 
         textfield.setBackground(new Color(25, 25, 25));
-        textfield.setBackground(new Color(25, 255, 0));
+        textfield.setForeground(new Color(25, 255, 0));
         textfield.setFont(new Font("Ink Free", Font.BOLD, 75));
         textfield.setHorizontalAlignment(JLabel.CENTER);
         textfield.setText("Tic-Tac-Toe");
@@ -46,13 +44,15 @@ public class main {
             buttons[i].setFont(new Font("MV Boli", Font.BOLD, 120));
             buttons[i].setFocusable(false);
             buttons[i].addActionListener(new ActionListener() {
+                @Override
                 public void actionPerformed(ActionEvent e) {
-                    for (int i = 0; i < 9; i++) {
-                        if (e.getSource() == buttons[i]) {
-                            if (buttons[i].getText().equals("")) {
-                                buttons[i].setText("X");
-                                turn = 1;
-                                checkForWin();
+                    if (!gameOver) {
+                        JButton button = (JButton) e.getSource();
+                        if (button.getText().equals("")) {
+                            button.setText("X");
+                            turnCount++;
+                            checkForWin();
+                            if (!gameOver) {
                                 aiMove();
                                 checkForWin();
                             }
@@ -69,6 +69,7 @@ public class main {
         } else {
             turn = 1;
             textfield.setText("AI's turn");
+            aiMove();
         }
 
         title_panel.add(textfield);
@@ -79,21 +80,13 @@ public class main {
     }
 
     public void aiMove() {
-        int choice;
-        do {
-            choice = random.nextInt(9);
-        } while (buttons[choice].getText().equals("X") || buttons[choice].getText().equals("O"));
-        buttons[choice].setText("O");
-        checkForWin();
-        checkForEnd("O");
-        if (turn == 1 && !gameOver) {
-            int index = random.nextInt(9);
-            while (buttons[index].getText().equals("X") || buttons[index].getText().equals("O")) {
-                index = random.nextInt(9);
-            }
-            buttons[index].setText("O");
-            turn = 0;
-            textfield.setText("Player's turn");
+        if (!gameOver) {
+            int choice;
+            do {
+                choice = random.nextInt(9);
+            } while (!buttons[choice].getText().equals(""));
+            buttons[choice].setText("O");
+            turnCount++;
         }
     }
 
@@ -104,30 +97,27 @@ public class main {
                     && buttons[i].getText().equals(buttons[i + 2].getText()) && !buttons[i].getText().equals("")) {
                 gameOver = true;
                 if (buttons[i].getText().equals("X")) {
-                    textfield.setText("Player wins!");
-                    playerScore++;
-                } else {
-                    textfield.setText("AI wins!");
-                    aiScore++;
+                    textfield.setText("Player wins");
+                    return;
                 }
             }
         }
-
         // check columns
-        for (int i = 0; i < 3; i++) {
-            if (buttons[i].getText().equals(buttons[i + 3].getText())
-                    && buttons[i].getText().equals(buttons[i + 6].getText()) && !buttons[i].getText().equals("")) {
+        for (int j = 0; j < 3; j++) {
+            if (buttons[j].getText().equals(buttons[j + 3].getText())
+                    && buttons[j].getText().equals(buttons[j + 6].getText()) && !buttons[j].getText().equals("")) {
                 gameOver = true;
-                if (buttons[i].getText().equals("X")) {
+                if (buttons[j].getText().equals("X")) {
                     textfield.setText("Player wins!");
                     playerScore++;
                 } else {
                     textfield.setText("AI wins!");
                     aiScore++;
                 }
+                updateScore();
+                return;
             }
         }
-
         // check diagonals
         if (buttons[0].getText().equals(buttons[4].getText()) && buttons[0].getText().equals(buttons[8].getText())
                 && !buttons[0].getText().equals("")) {
@@ -139,8 +129,9 @@ public class main {
                 textfield.setText("AI wins!");
                 aiScore++;
             }
+            updateScore();
+            return;
         }
-
         if (buttons[2].getText().equals(buttons[4].getText()) && buttons[2].getText().equals(buttons[6].getText())
                 && !buttons[2].getText().equals("")) {
             gameOver = true;
@@ -151,146 +142,62 @@ public class main {
                 textfield.setText("AI wins!");
                 aiScore++;
             }
+            updateScore();
+            return;
         }
-
         // check for tie
-        if (turnCount == 9 && !gameOver) {
+        if (turnCount == 9) {
             gameOver = true;
             textfield.setText("It's a tie!");
+            updateScore();
         }
     }
 
-    public void aiTurn() {
-        int index = -1;
-        int[] possibleMoves = new int[9];
-
-        for (int i = 0; i < 9; i++) {
-            if (buttons[i].getText().equals("")) {
-                possibleMoves[++index] = i;
+    public void updateScore() {
+        textfield.setText("Player: " + playerScore + "   AI: " + aiScore);
+        int delay = 1000; // milliseconds
+        ActionListener taskPerformer = new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                resetGame();
             }
+        };
+        if (playerScore >= 3 || aiScore >= 3) {
+            Timer timer = new Timer(delay, taskPerformer);
+            timer.setRepeats(false);
+            timer.start();
         }
-
-        int move = -1;
-        if (index == 0) {
-            move = possibleMoves[0];
-        } else {
-            for (int i = 0; i < possibleMoves.length; i++) {
-                if (possibleMoves[i] != 0) {
-                    buttons[possibleMoves[i]].setText("");
-                }
-            }
-
-            while (move == -1) {
-                int random = new Random().nextInt(index + 1);
-                int currentMove = possibleMoves[random];
-                buttons[currentMove].setText("O");
-                if (checkForEnd("O")) {
-                    move = currentMove;
-                } else {
-                    buttons[currentMove].setText("");
-                    possibleMoves[random] = 0;
-                    if (possibleMoves[0] == 0) {
-                        move = possibleMoves[1];
-                    }
-                }
-            }
-        }
-
-        buttons[move].setText("O");
-        turnCount++;
-        currentTurn = 0;
-    }
-
-    public boolean checkForEnd(String player) {
-        boolean result = false;
-
-        // check for row wins
-        if (buttons[0].getText().equals(player) && buttons[1].getText().equals(player)
-                && buttons[2].getText().equals(player)) {
-            result = true;
-        } else if (buttons[3].getText().equals(player) && buttons[4].getText().equals(player)
-                && buttons[5].getText().equals(player)) {
-            result = true;
-        } else if (buttons[6].getText().equals(player) && buttons[7].getText().equals(player)
-                && buttons[8].getText().equals(player)) {
-            result = true;
-        }
-
-        // check for column wins
-        else if (buttons[0].getText().equals(player) && buttons[3].getText().equals(player)
-                && buttons[6].getText().equals(player)) {
-            result = true;
-        } else if (buttons[1].getText().equals(player) && buttons[4].getText().equals(player)
-                && buttons[7].getText().equals(player)) {
-            result = true;
-        } else if (buttons[2].getText().equals(player) && buttons[5].getText().equals(player)
-                && buttons[8].getText().equals(player)) {
-            result = true;
-        }
-
-        // check for diagonal wins
-        else if (buttons[0].getText().equals(player) && buttons[4].getText().equals(player)
-                && buttons[8].getText().equals(player)) {
-            result = true;
-        } else if (buttons[2].getText().equals(player) && buttons[4].getText().equals(player)
-                && buttons[6].getText().equals(player)) {
-            result = true;
-        }
-
-        // check for tie
-        else if (!buttons[0].getText().equals("") && !buttons[1].getText().equals("")
-                && !buttons[2].getText().equals("")
-                && !buttons[3].getText().equals("") && !buttons[4].getText().equals("")
-                && !buttons[5].getText().equals("")
-                && !buttons[6].getText().equals("") && !buttons[7].getText().equals("")
-                && !buttons[8].getText().equals("")) {
-            result = true;
-            textfield.setText("It's a tie!");
-        }
-
-        return result;
     }
 
     public void resetGame() {
         for (int i = 0; i < 9; i++) {
+            buttons[i].setEnabled(true);
             buttons[i].setText("");
         }
-        gameOver = false;
-        textfield.setText("Tic-Tac-Toe");
-    }
-
-    public void updateScoreboard() {
-        textfield.setText("Player: " + playerScore + " | AI: " + aiScore);
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        for (int i = 0; i < 9; i++) {
-            if (e.getSource() == buttons[i]) {
-                if (!gameOver) {
-                    if (buttons[i].getText().equals("")) {
-                        buttons[i].setText("X");
-                        checkForWin();
-                        checkForEnd("X");
-                        if (!gameOver) {
-                            aiMove();
-                        }
-                    }
-                } else {
-                    int choice = JOptionPane.showOptionDialog(frame, "Play again?", "Game over",
-                            JOptionPane.YES_NO_OPTION,
-                            JOptionPane.QUESTION_MESSAGE, null, null, null);
-                    if (choice == JOptionPane.YES_OPTION) {
-                        resetGame();
-                    } else {
-                        System.exit(0);
-                    }
-                }
+        if (playerScore >= 3 || aiScore >= 3) {
+            if (playerScore > aiScore) {
+                textfield.setText("Player wins the match!");
+            } else if (aiScore > playerScore) {
+                textfield.setText("AI wins the match!");
+            } else {
+                textfield.setText("The match is tied!");
             }
+            playerScore = 0;
+            aiScore = 0;
+        }
+        turnCount = 0;
+        gameOver = false;
+        int randomNum = random.nextInt(2);
+        if (randomNum == 0) {
+            turn = 0;
+            textfield.setText("Player's turn");
+        } else {
+            turn = 1;
+            textfield.setText("AI's turn");
+            aiMove();
         }
     }
 
     public static void main(String[] args) {
         new main();
     }
-
 }
